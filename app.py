7a16784a191f3d3c79592801965576a7bc11b068
Motoaki Tanaka <katachiwotodomete@gmail.com>
2023-06-18 09:55:14 +0000

    add whisper

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import whisper
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'index page'
+
+@app.route('/whisper')
+def transcribe():
+    model = whisper.load_model("base")
+    result = model.transcribe("audio.mp3")
+    print(result["text"])
+    return 'whisper'
 
 if __name__ == '__main__':
     app.run()
